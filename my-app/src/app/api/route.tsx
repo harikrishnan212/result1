0a67ea2239740c6e6863073e88ea950f5e3dad89
harikrishnan212 <krishnanhari212005@gmail.com>
2025-07-24 10:52:10 +0000

    hello

--- my-app/src/app/api/route.tsx
+++ my-app/src/app/api/route.tsx
@@ -1,19 +1,7 @@
 export async function GET(req: Request) {
-  try {
-    return new Response(
-      JSON.stringify({ data: "Hello world" }),
-      {
-        status: 400,
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({ data: "no" }),
-      {
-        status: 500,
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-  }
+ 
+
+  return Response.json({
+    Data:"hello world"
+  })
 }
